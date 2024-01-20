ee6b0870801b5d40dd44285ca41e434cab4ee617
Manik chopra <129728730+manik031@users.noreply.github.com>
2024-01-21 00:42:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -322,5 +322,5 @@ def generate_image():
         return jsonify({"success": False, "error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
